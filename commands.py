c010e7306e30b76763cf3089b27ea99d36b86849
shannellemibei <shannelle@solutech.co.ke>
2025-07-02 09:23:19 +0300

    commands update

--- commands.py
+++ commands.py
@@ -2,6 +2,6 @@ COMMANDS={
   "about solutech": "Solutech Limited is a custom software development company founded in 2014 by 4 entrepreneurs with a high competence in Technology.",
   "what do you do": "I help you with tasks such as voice recognition, answering questions, and providing information.",
   "who created you": "Shannelle Mibei, a Data Scientist at Solutech Limited",
-  "how are you":"I'm doing great, thank you for asking!"
+  "how are you": "I'm doing great, thank you for asking!"
 
 }
